cc703ff251cebf278d2cfea8b56f05884be48bbd
Malika <dusmagambetovaaa@gmail.com>
2026-03-13 14:44:23 +0500

    feat: add POST, GET, DELETE tasks without chi

---
+++ main.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Task struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Done bool `json:"done"`
+	CreatedAt time.Time `json:"created_at"`
+	Password string `json:"-"`
+	Priority string `json:"priority"`
+}
+
+type CreateTaskRequest struct {
+	Title string `json:"title" validate:"required,min=3"`
+	Priority string `json:"priority"`
+}
+
+var tasks = []Task{
+	{ID: 1, Title: "learn golang", Done: false, CreatedAt: time.Now(), Priority: "low"},
+	{ID: 2, Title: "buy milk", Done: true, Priority: "low"},
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/tasks", tasksHandler)
+	http.HandleFunc("/tasks/", taskHandler)
+	log.Println("Server running on :8085")
+	log.Fatal(http.ListenAndServe(":8085", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, I am your backend")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "ok")
+}
+
+func tasksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet: getTasks(w, r)
+	case http.MethodPost: createTask(w, r)
+	default: http.Error(w, "method  not allowed", http.StatusMethodNotAllowed)
+	} 
+}
+
+func taskHandler(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/tasks/") // or id := r.URL.Path[len("/tasks/"):]
+
+	if id == "" {
+		http.Error(w, "task id required", http.StatusBadRequest) //400
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		getTask(w, r, id)
+	case http.MethodDelete:
+		deleteTask(w, r, id)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+var validate = validator.New()
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var req CreateTaskRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid json", http.StatusBadRequest)
+		return 
+	}
+
+	err = validate.Struct(req)
+	if err != nil {
+		http.Error(w, "invalid input", http.StatusBadRequest) //400
+		return
+	}
+
+	priority := req.Priority
+
+	if priority == "" {
+		priority = "low"
+	}
+
+	task := Task{
+		ID: len(tasks) + 1,
+		Title: req.Title,
+		Done: false,
+		CreatedAt: time.Now(),
+		Priority: priority,
+	}
+
+	tasks = append(tasks, task)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(task)
+} 
+
+func deleteTask(w http.ResponseWriter, r *http.Request, id string) {
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+	for i, t := range tasks {
+		if t.ID == idInt {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			w.WriteHeader(http.StatusNoContent) //delete 204
+			return
+		}
+	}
+	http.Error(w, "task not found", http.StatusNotFound)
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request, id string) {
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, "error", http.StatusBadRequest)
+		return
+	}
+	for _, t := range tasks {
+		if idInt == t.ID {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(t)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+}
+
+/*
+package main
+
+import (
+ "context"
+ "encoding/json"
+ "log"
+ "net/http"
+
+ "github.com/jackc/pgx/v5"
+)
+
+var conn *pgx.Conn
+
+type User struct {
+ ID   int    json:"id"
+ Name string json:"name"
+}
+
+func main() {
+ var err error
+ conn, err = pgx.Connect(context.Background(),
+  "postgres://postgres:password@localhost:5432/mydb")
+ if err != nil {
+  log.Fatal(err)
+ }
+
+ http.HandleFunc("/users", usersHandler)
+
+ log.Println("Server started on :8080")
+ log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+ switch r.Method {
+
+ case http.MethodGet:
+  getUsers(w)
+
+ case http.MethodPost:
+  createUser(w, r)
+
+ default:
+  http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+ }
+}
+
+func getUsers(w http.ResponseWriter) {
+ rows, err := conn.Query(context.Background(),
+  "SELECT id, name FROM users")
+ if err != nil {
+  http.Error(w, err.Error(), 500)
+  return
+ }
+ defer rows.Close()
+
+ var users []User
+
+ for rows.Next() {
+  var u User
+  err := rows.Scan(&u.ID, &u.Name)
+  if err != nil {
+   http.Error(w, err.Error(), 500)
+   return
+  }
+  users = append(users, u)
+ }
+
+ json.NewEncoder(w).Encode(users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+ var u User
+
+ err := json.NewDecoder(r.Body).Decode(&u)
+ if err != nil {
+  http.Error(w, err.Error(), 400)
+  return
+ }
+
+ err = conn.QueryRow(context.Background(),
+  "INSERT INTO users (name) VALUES ($1) RETURNING id",
+  u.Name).Scan(&u.ID)
+ if err != nil {
+  http.Error(w, err.Error(), 500)
+  return
+ }
+
+ w.WriteHeader(http.StatusCreated)
+ json.NewEncoder(w).Encode(u)
+}
+*/
